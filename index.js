810e9b59cf865b381dc9a3cf99e6ebb40af14bf9
Jonah <jonahtkane@gmail.com>
2023-02-11 17:01:32 -0600

    more code added - tutor session data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 // require 
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -9,14 +12,26 @@ const questions = [{
 
 }  
 ];
+
 // refer to read me file "when i..."
 
-
+const fileName = "test.md"
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{
+        err ? console.error(err) : console.log('Commit logged!')
+
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer.prompt(questions) 
+.then(function(data){
+    console.log(data);
+    writeToFile(fileName, generateMarkdown(data))
+})
 
+}
 // Function call to initialize app
 init();
