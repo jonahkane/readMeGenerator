ef21a89dcd3b4ca54396ce15fa2d85772572cfda
Jonah <jonahtkane@gmail.com>
2023-02-11 16:25:16 -0600

    added starter code and first question

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+// require 
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+
+}  
+];
+// refer to read me file "when i..."
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
