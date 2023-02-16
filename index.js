a5255a4f2ea1a678c8647a321e978769e045d6a0
Jonah <jonahtkane@gmail.com>
2023-02-16 10:50:51 -0600

    added additional questions

--- index.js
+++ index.js
@@ -9,8 +9,59 @@ const questions = [{
     type: 'input',
     message: 'What is the title of your project?',
     name: 'title',
+},  
+{
+    type:'input',
+    message:"Please write a short description of your project",
+    name: "description"
+},
+{
+    type:'input',
+    message: "Are there any installation instructions for this project?",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "What does the user need to know about using the repository?",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "What does the user need to know about contributing to the repository?",
+    name: "contribute"
+},
+{
+    type:'input',
+    message: "What command should be run to run tests?",
+    name: "npm run test"
+},
+{
+    type:'list',
+    message: "Which license do you want your project to have?",
+    name: "license",
+    choices: [
+        "MIT",
+        "Unlicense",
+        "Apache 2.0",
+        "GNU v3",
+        "BSD 3-Clause",
+        "Mozilla Public License 2.0"
+    ]
+},
 
-}  
+
+
+
+{
+    type:'input',
+    message: "what is your GitHUb user name?",
+    name: "username"
+},
+{
+    type:'input',
+    message: "what is your email address?",
+    name: "email"
+},
 ];
 
 // refer to read me file "when i..."
