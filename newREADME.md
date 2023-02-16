ac51c7d9d09585c07af57ca30ec74791cf3465db
Jonah <jonahtkane@gmail.com>
2023-02-16 16:13:24 -0600

    cleaned up code a bit.  deleted test file

--- newREADME.md
+++ newREADME.md
@@ -1,10 +1,10 @@
-# 1
+# Cool Calendar
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
   *****somehow need to get the license badge to appear here
   ## Description 
 
- 2
+ daily planner that allows the user to add events 
  
   ## Table of contents 
 
@@ -16,24 +16,25 @@
   - [Questions](#questions)
   
   ## Installation
-  3
+  no - but maybe npm i 
 
   ## Usage
-  4
+  use it how you want to.
 
   ## Contribute 
-  5
+  this program was written entirely by me, jonah kane, but with the help of tutoring sessions, google, UofM bootcamp course/tas and help from aother students too
 
   ## Tests 
-  6
+  npm run test is a good idea to use
 
   ## License 
-  MIT License
-  (https://opensource.org/licenses/MIT)
+  Apache License 2.0
+  (https://opensource.org/licenses/Apache-2.0)
+  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
   - Please refer to the license information in the repo if applicable.
 
   ## Questions
-  If you have questions on this project you can find me on Github at https://github.com/7
-  or please feel free to email me at 8
+  If you have questions on this project you can find me on Github at https://github.com/jonahkane
+  or please feel free to email me at jonahtkane@gmail.com
 
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,38 +7,38 @@ function renderLicenseBadge(license) {
 if (license === "None") {
   return ``;
 } else if (license === "Apache License 2.0") {
-  return `[![${license}](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  return `[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`;
 } else if (license === "GNU General Public License v3.0") {
-  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
 } else if (license === "MIT License") {
-  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
 
 } else if (license === "BSD 2-Clause 'Simplified' license") {
-  return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+  return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]`;
 
 } else if (license === "BSD 3-Clause 'New' or 'Revised' License") {
-  return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`;
 
 } else if (license === "Boost Software License 1.0") {
-  return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+  return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]`;
 
 } else if (license === "Creative Commons Zero v1.0 Universal") {
-  return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+  return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)]`;
 
 } else if (license === "Eclipse Public License 2.0") {
-  reutrn `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+  reutrn `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]`;
 
 } else if (license === "GNU Affero General Public License v3.0") {
-  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
 
 } else if (license === "GNU General Public License v2.0") {
-  return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]`;
 
 } else if (license === "Mozilla Public License 2.0") {
-  return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]`;
 
 } else if (license === "The Unlicense") {
-  return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+  return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]`;
 }
 }
 
@@ -127,7 +127,6 @@ function renderLicenseSection(license) {
   return `# ${data.title}
 
 ${renderLicenseBadge(license)}
-  *****somehow need to get the license badge to appear here
   ## Description 
 
  ${data.description}
@@ -145,7 +144,9 @@ ${renderLicenseBadge(license)}
   ${data.installation}
 
   ## Usage
-  ${data.usage}
+  ${data.usage} \n
+  here is a great place for you to upload screen shots or even a screen record of your application.  But remember to delete this line of text first
+  
 
   ## Contribute 
   ${data.contribute}
